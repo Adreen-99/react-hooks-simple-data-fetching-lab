378b20bcc8ea6ac370f9d42531415b72215a1e86
+ <+githinjiadreen27@gmail.com>
2025-07-25 21:42:52 +0300

    Fetching Data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import {useState, useEffect} from 'react'
+
+function App() {
+    const URL = `https://dog.ceo/api/breeds/image/random`
+    const [dogImage, setDogImage] = useState(null);
+
+    useEffect(() => {
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => setDogImage(data.message))
+            .catch(error => console.error('Error fetching data:', error));
+    }, [URL])
+
+  if (!dogImage) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={dogImage} alt="A Random Dog" />;
+}
+  
+
+export default App
